76b31962088ceb7abbfb9fd1b94ed6e6a98ee8c6
leelambicak <leelambicak@gmail.com>
2024-09-12 09:45:10 +0530

    my first commit

---
+++ src/main/java/com/example/mydemogithub/BankAccount.java
@@ -0,0 +1,74 @@
+package com.example.mydemogithub;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class BankAccount {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String userName;
+
+    private String phoneNo;
+
+    private String address;
+
+    public BankAccount(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public BankAccount( String userName, String phoneNo, String address) {
+    
+        this.userName = userName;
+        this.phoneNo = phoneNo;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount [id=" + id + ", userName=" + userName + ", phoneNo=" + phoneNo + ", address=" + address
+                + ", getId()=" + getId() + ", getUserName()=" + getUserName() + ", getPhoneNo()=" + getPhoneNo()
+                + ", getAddress()=" + getAddress() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+                + ", toString()=" + super.toString() + "]";
+    }
+
+    
+   
+  
+}
---
+++ src/test/java/com/example/mydemogithub/MydemogithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydemogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemogithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
